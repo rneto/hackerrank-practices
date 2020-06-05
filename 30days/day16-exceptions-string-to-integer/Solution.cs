91731c3fbc83b65c399140768fa6df637e328919
rneto <rrnoak@gmail.com>
2020-06-05 10:42:00 +0200

    feat: Solve day 16 of 30 Days of Code practice.

--- 30days/day16-exceptions-string-to-integer/Solution.cs
+++ 30days/day16-exceptions-string-to-integer/Solution.cs
@@ -1,2 +1,19 @@
 // PROBLEM: https://www.hackerrank.com/challenges/30-exceptions-string-to-integer/problem
 
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+class Solution {
+
+    static void Main(String[] args) {
+        string S = Console.ReadLine();
+
+        try {
+            Console.WriteLine(int.Parse(S));
+        }
+        catch(Exception) {
+            Console.WriteLine("Bad String");
+        }
+    }
+}
