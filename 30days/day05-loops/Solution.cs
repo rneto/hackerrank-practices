987889dafa9d0804df0e40b52c772fc952e30cd7
rneto <rrnoak@gmail.com>
2020-06-06 10:56:55 +0200

    feat: Add solutions for 30 Days of Code practice.

---
+++ 30days/day05-loops/Solution.cs
@@ -0,0 +1,28 @@
+// PROBLEM: https://www.hackerrank.com/challenges/30-loops/problem
+
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Solution {
+
+
+
+    static void Main(string[] args) {
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        for(int i = 1; i <= 10; i++) {
+            Console.WriteLine($"{n} x {i} = {n * i}");
+        }
+    }
+}
