a67852787fbf114afc01a063ea19b3860a771970
rneto <rrnoak@gmail.com>
2020-07-03 09:39:42 +0200

    feat: Solve Counting Valleys and Jumping on the Clouds Challenges

---
+++ interview-preparation-kit/warmup/counting-valleys/Solution.js
@@ -0,0 +1,54 @@
+// PROBLEM: https://www.hackerrank.com/challenges/counting-valleys/problem?h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=warmup
+
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+// Complete the countingValleys function below.
+function countingValleys(n, s) {
+    let altitude = 0;
+    let valleys = 0;
+    for (let i = 0; i < n; i++){
+        if (altitude === -1 && s[i] === 'U') {
+            valleys++;
+        }
+        altitude += (s[i] === 'U' ? 1 : -1);
+    }
+    return valleys;
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const n = parseInt(readLine(), 10);
+
+    const s = readLine();
+
+    let result = countingValleys(n, s);
+
+    ws.write(result + "\n");
+
+    ws.end();
+}
---
+++ interview-preparation-kit/warmup/jumping-on-the-clouds/Solution.js
@@ -0,0 +1,58 @@
+// PROBLEM: https://www.hackerrank.com/challenges/jumping-on-the-clouds/problem?h_l=interview&playlist_slugs%5B%5D%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D%5B%5D=warmup&isFullScreen=true&h_r=next-challenge&h_v=zen
+
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+// Complete the jumpingOnClouds function below.
+function jumpingOnClouds(c) {
+    let jumps = 0;
+    let j = 0;
+    const clouds = c.length;
+    while (j < clouds - 1){
+        if (c[j + 2] === 0){
+            j+=2;
+        }
+        else {
+            j++;
+        }
+        jumps++;
+    }
+    return jumps;
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const n = parseInt(readLine(), 10);
+
+    const c = readLine().split(' ').map(cTemp => parseInt(cTemp, 10));
+
+    let result = jumpingOnClouds(c);
+
+    ws.write(result + "\n");
+
+    ws.end();
+}
