436f4c7f02f1882001a13faf6789cb579c485cdf
rneto <rrnoak@gmail.com>
2020-07-07 09:12:11 +0200

    feat: Add repeated string challenge solution

---
+++ interview-preparation-kit/warmup/repeated-string/Solution.js
@@ -0,0 +1,50 @@
+// PROBLEM: https://www.hackerrank.com/challenges/repeated-string/problem?h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=warmup
+
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+// Complete the repeatedString function below.
+function repeatedString(s, n) {
+    const as = [...s].filter(c => c === 'a').length;
+    const remainder = n % s.length;
+    const entireStrings = ((n - remainder)/s.length);
+
+    return (as * entireStrings) + [...s.substring(0, remainder)].filter(c => c === 'a').length;
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const s = readLine();
+
+    const n = parseInt(readLine(), 10);
+
+    let result = repeatedString(s, n);
+
+    ws.write(result + "\n");
+
+    ws.end();
+}
